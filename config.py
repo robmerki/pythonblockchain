d878aa77292c4c36044bb0902ba53165bd9e1a82
Robert Merki <rob@merki.ca>
2017-11-08 21:24:28 -0800

    started pt2 of tutorial + added config

---
+++ config.py
@@ -0,0 +1,6 @@
+CHAINDATA_DIR = 'chaindata/'
+BROADCASTED_BLOCK_DIR = CHAINDATA_DIR + 'bblocs/'
+
+NUM_ZEROS = 5 # this is the difficulty currently
+
+BLOCK_VAR_CONVERSIONS = { 'index':int, 'nonce':int, 'hash':str, 'prev_hash':str, 'timestamp':int }
